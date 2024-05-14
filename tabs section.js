f6d497f21bd4e942a80b19119a3583a18162ea40
Abdul Khan <149056379+Abdulkhan9786@users.noreply.github.com>
2024-05-14 16:57:44 +0530

    Update tabs section.js

--- tabs section.js
+++ tabs section.js
@@ -38,6 +38,44 @@ $(navButtons).click(function(e){
   boxToShow.show('300');
   boxToShow.find('*').show();
 });
+
+
+
+
+
+
+Link to current tab change
+Link URL /page name#Tab-1(tab setting name)
+<script>
+  $( function() {
+    function changeTab() {
+        var tabName = window.location.hash.substr(1);
+        var tabTarget = $('[data-w-tab="' + tabName + '"]');
+        if (tabTarget.length) {
+            tabTarget.click();
+        }
+    }
+    jQuery('[data-w-tab]').each(function(){
+      var $this = $(this);
+      var dataWTabValu = jQuery($this).attr('data-w-tab');
+      var pargedDataTab = dataWTabValu.replace(/\s+/g,"-");
+      jQuery($this).attr('data-w-tab', pargedDataTab);
+    });
+
+    //when page is first loaded
+    if(window.location.hash){
+        changeTab();
+    }
+
+    //internal page linking
+    $(window).on('hashchange', changeTab);
+
+    $('[data-w-tab]').on('click', function(){
+        history.pushState({}, '', '#'+$(this).data("w-tab"));
+    });
+    
+});
+  </script>
   
 //onload, make first button active
 navButtons.first().addClass('active');
